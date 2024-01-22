743071057eec2bf454e2168b511e96009c803c8b
Antonio Bruce Webb <120767438+OrganiSoftware@users.noreply.github.com>
2024-01-22 07:44:32 -0600

    Add files via upload

---
+++ runQEMU.py
@@ -0,0 +1,15 @@
+import argparse
+import os
+
+class QEMU:
+    def runQEMU(self):
+        parser = argparse.ArgumentParser(description='Create login login credetials for user rsa_auth')
+        parser.add_argument('image_path', help='path to QEMU image')
+        parser.add_argument('format', help='image format')
+        parser.add_argument('accell', help='image accellerator')
+        args = parser.parse_args()
+        os.system("qemu-system-x86_64 -accel accel="+str(args.accell)+" -m 54G -smp 32 -boot order=d  -blockdev node-name=prot-node,driver=file,filename="+str(args.image_path)+" -blockdev node-name=fmt-node,driver="+str(args.format)+",file=prot-node  -device virtio-blk,drive=fmt-node,share-rw=on -device virtio-sound")
+
+if __name__ == '__main__':
+    runQEMU = QEMU()
+    runQEMU.runQEMU()
