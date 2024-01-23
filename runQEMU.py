ea41025ec379e825a9aac9ac00585533e7239475
Antonio Webb <120767438+OrganiSoftware@users.noreply.github.com>
2024-01-22 19:56:27 -0600

    fixed script added params for cores and mem

--- runQEMU.py
+++ runQEMU.py
@@ -6,9 +6,14 @@ class QEMU:
         parser = argparse.ArgumentParser(description='Create login login credetials for user rsa_auth')
         parser.add_argument('image_path', help='path to QEMU image')
         parser.add_argument('format', help='image format')
-        parser.add_argument('accell', help='image accellerator')
+        parser.add_argument('accell', help='image accelerator')
+        parser.add_argument('cores', help='num cores')
+        parser.add_argument('mem', help='ram')
         args = parser.parse_args()
-        os.system("qemu-system-x86_64 -accel accel="+str(args.accell)+" -m 54G -smp 32 -boot order=d  -blockdev node-name=prot-node,driver=file,filename="+str(args.image_path)+" -blockdev node-name=fmt-node,driver="+str(args.format)+",file=prot-node  -device virtio-blk,drive=fmt-node,share-rw=on -device virtio-sound")
+        os.system("qemu-system-x86_64 -accel accel="+str(args.accell)+" -m "+str(args.mem)+" -smp "+str(args.cores)
+                  +" -boot order=d  -blockdev node-name=prot-node,driver=file,filename="+str(args.image_path)
+                  +" -blockdev node-name=fmt-node,driver="+str(args.format)
+                  +",file=prot-node  -device virtio-blk,drive=fmt-node,share-rw=on -device virtio-sound")
 
 if __name__ == '__main__':
     runQEMU = QEMU()
